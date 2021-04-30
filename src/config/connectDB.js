32ea542e83af52544a37008299f47b181cbdc15f
cuongdang <cuongdang@tenomad.com>
2021-04-30 16:04:49 +0700

    Update config

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -9,7 +9,7 @@ let connectDB=()=>{
     let DB_USERNAME="";
     let DB_PASSWORD="";
 
-    const URI = "mongodb+srv://root:aPPPHXkPTWetoE6k@cluster0.y9j7a.mongodb.net/message";
+    const URI = "mongodb+srv://root:aPPPHXkPTWetoE6k@cluster0-shard-00-00.y9j7a.mongodb.net:27017,cluster0-shard-00-01.y9j7a.mongodb.net:27017,cluster0-shard-00-02.y9j7a.mongodb.net:27017/message?ssl=true&replicaSet=atlas-v7mhqe-shard-0&authSource=admin&retryWrites=true&w=majority";
 
     //let URI=`${DB_CONNECTION}://${DB_HOST}:${DB_PORT}/${DB_NAME}?retryWrites=true&w=majority`;
 
--- src/config/session.js
+++ src/config/session.js
@@ -9,7 +9,7 @@ let DB_NAME="message";
 let DB_USERNAME="";
 let DB_PASSWORD="";
 let sessionStore=new MongoStore({
-    url: "mongodb+srv://root:aPPPHXkPTWetoE6k@cluster0.y9j7a.mongodb.net/message",
+    url: "mongodb+srv://root:aPPPHXkPTWetoE6k@cluster0-shard-00-00.y9j7a.mongodb.net:27017,cluster0-shard-00-01.y9j7a.mongodb.net:27017,cluster0-shard-00-02.y9j7a.mongodb.net:27017/message?ssl=true&replicaSet=atlas-v7mhqe-shard-0&authSource=admin&retryWrites=true&w=majority",
     autoConnect:true,
     // autoRemove:"native"
 })
