2d8b797f614a128bb2532f67ea180fef19ffed42
cuongdang <cuongdang@tenomad.com>
2021-04-30 15:23:30 +0700

    update config

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,14 +2,16 @@ import mongoose from "mongoose";
 import bluebird from 'bluebird'
 let connectDB=()=>{
     mongoose.Promise=bluebird;
-    let DB_CONNECTION="mongodb";
-    let DB_HOST="localhost";
-    let DB_PORT="27017";
-    let DB_NAME="awesome_chat";
+    let DB_CONNECTION="mongodb+srv";
+    let DB_HOST="root";
+    let DB_PORT="root@12345!@cluster0.y9j7a.mongodb.net";
+    let DB_NAME="message";
     let DB_USERNAME="";
     let DB_PASSWORD="";
 
-    let URI=`${DB_CONNECTION}://${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+    //const uri = "mongodb+srv://root:<password>@cluster0.y9j7a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+    let URI=`${DB_CONNECTION}://${DB_HOST}:${DB_PORT}/${DB_NAME}?retryWrites=true&w=majority`;
 
     return mongoose.connect(URI,{useNewUrlParser: true });
 
