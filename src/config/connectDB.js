2edff476ce0177ccc8d94290cb9b338fe8869708
cuongdang <cuongdang@tenomad.com>
2021-04-30 15:50:05 +0700

    Update config

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -9,7 +9,7 @@ let connectDB=()=>{
     let DB_USERNAME="";
     let DB_PASSWORD="";
 
-    const URI = "mongodb+srv://root:root@12345!@cluster0.y9j7a.mongodb.net/message?retryWrites=true&w=majority";
+    const URI = "mongodb+srv://root:aPPPHXkPTWetoE6k@cluster0.y9j7a.mongodb.net/message?retryWrites=true&w=majority";
 
     //let URI=`${DB_CONNECTION}://${DB_HOST}:${DB_PORT}/${DB_NAME}?retryWrites=true&w=majority`;
 
--- src/config/session.js
+++ src/config/session.js
@@ -9,7 +9,7 @@ let DB_NAME="message";
 let DB_USERNAME="";
 let DB_PASSWORD="";
 let sessionStore=new MongoStore({
-    url: "mongodb+srv://root:root@12345!@cluster0.y9j7a.mongodb.net/message?retryWrites=true&w=majority",
+    url: "mongodb+srv://root:aPPPHXkPTWetoE6k@cluster0.y9j7a.mongodb.net/message?retryWrites=true&w=majority",
     autoConnect:true,
     // autoRemove:"native"
 })
